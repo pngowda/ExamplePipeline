f389c31071cc5440d058ff90e58088df10aca95c
prajwal <pgowda@itemis.de>
2019-07-27 17:54:23 +0200

    jenkins artifactory example

---
+++ maths/src/test/java/com/demo/javaprojects/maths/MathTest.java
@@ -0,0 +1,61 @@
+
+package com.demo.javaprojects.maths;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MathTest {
+	Math math;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("@Before Inside setUp");
+		math = new Math();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("@After Inside tearDown\n*********************************");
+		math = null;
+	}
+
+	@Test
+	public void addTest() {
+		System.out.println("@Test Inside addTest");
+		
+		assertEquals(5, math.add(2, 3));
+	}
+	
+	@Test
+	public void subtractTest() {
+		System.out.println("@Test Inside subtractTest");
+		
+		assertEquals(5, math.subtract(7, 2));
+	}
+	
+	@Test
+	public void multiplyTest() {
+		System.out.println("@Test Inside multiplyTest");
+		
+		assertEquals(15, math.multiply(5, 3));
+	}
+	
+	@Test
+	public void divideTest() {
+		System.out.println("@Test Inside divideTest");
+		
+		assertEquals(5, math.divide(20, 4));
+		assertNotEquals(5.5, math.divide(11, 2)); // to check it's integer division
+	}
+
+}
